c600537fb1ef28e952451086bb0bd2c068e07937
fielachQT <fielachQT@FIELACHQT>
2023-02-10 21:50:45 +0400

    Добавьте файлы проекта.

---
+++ Moni/Model/Usage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreHardwareMonitor;
+using LibreHardwareMonitor.Hardware;
+
+namespace Moni.Model
+{
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+
+    public class Usage
+    {
+        public void Monitor()
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+
+            foreach (var hardware in computer.Hardware)
+            {
+                if (hardware)
+                {
+
+                }
+            }
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+            computer.Close();
+        }
+    }
+
+    
+
+
+}
